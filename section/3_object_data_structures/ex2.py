86608096165fcea8120f18463e0a51102156a6e6
namntran <namtran78@gmail.com>
2019-07-12 06:10:53 +1000

    indexing and string slicing

---
+++ section/3_object_data_structures/ex2.py
@@ -0,0 +1,11 @@
+mystring = "abcdefghijk"
+print(mystring[2])
+print(mystring[2:])
+print(mystring[:3])
+print(mystring[3:6])
+print(mystring[1:3])
+print(mystring)
+print(mystring[::])
+print(mystring[::2]) #step size of 2
+print(mystring[::3]) #step size of 3
+print(mystring[2:7:2]) #start index 2, stop index 7, step size of 2
